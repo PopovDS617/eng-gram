9085ebce42951bf24ecb09a25fa61dae4036f47a
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2022-11-17 10:35:43 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <PageLayout>
     <div className="flex flex-col justify-start items-center h-full">
-      <h1 className="font-segoeBold text-3xl md:text-6xl text-customBlue h-1/6 pt-5 ">
+      <h1 className="font-segoeBold text-3xl md:text-7xl text-customBlue h-1/6 pt-5 text-center ">
         English grammar simplified
       </h1>
-      <ul className="text-customBlue font-segoeNorm text-2xl md:text-6xl">
+      <ul className="text-customBlue font-segoeNorm text-2xl md:text-5xl mt-20">
         <Link href="/basic">
           <li className="mt-10 hover:scale-105 transition-all duration-3000 cursor-pointer">
             Basic
