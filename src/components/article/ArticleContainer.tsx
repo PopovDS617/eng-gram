ab6a21c8d0e17544076cc40eff1608ab2107d7fc
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2022-11-17 10:20:33 +0300

    Update ArticleContainer.tsx

--- src/components/article/ArticleContainer.tsx
+++ src/components/article/ArticleContainer.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const ArticleContainer = (props: Props) => {
   return (
-    <div className="w-full flex flex-col text-white items-center justify-start mt-10 mb-10">
+    <div className="w-full flex flex-col text-white items-center justify-start mt-10 mb-20">
       {props.children}
     </div>
   );
