b5c29acb89495b9156daab0a1725e7bc55dd460f
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2022-11-29 15:34:41 +0300

    Update advanced.tsx

--- src/pages/advanced.tsx
+++ src/pages/advanced.tsx
@@ -13,37 +13,37 @@ const AdvancedPage = (props: Props) => {
         <PageList>
           <ul className="  text-3xl ">
             <Link href="intermediate/aaa">
-              <li className="list-circle text-3xl hover:text-customBlue transition-all duration-100 cursor-pointer mt-4">
+              <li className="list-circle text-3xl hover:text-customBlue hover:list-disc transition-all duration-100 cursor-pointer mt-4">
                 Passive voice
               </li>
             </Link>
             <Link href="intermediate/aaa">
-              <li className="list-circle  hover:text-customBlue transition-all duration-100 cursor-pointer mt-4">
+              <li className="list-circle  hover:text-customBlue hover:list-disc transition-all duration-100 cursor-pointer mt-4">
                 Reported speech
               </li>
             </Link>
             <Link href="intermediate/aaa">
-              <li className="list-circle   hover:text-customBlue transition-all duration-100 cursor-pointer mt-4">
+              <li className="list-circle   hover:text-customBlue hover:list-disc transition-all duration-100 cursor-pointer mt-4">
                 Relative clause
               </li>
             </Link>
             <Link href="intermediate/aaa">
-              <li className="list-circle   hover:text-customBlue transition-all duration-100 cursor-pointer mt-4">
+              <li className="list-circle   hover:text-customBlue hover:list-disc transition-all duration-100 cursor-pointer mt-4">
                 Modal verbs
               </li>
             </Link>
             <Link href="intermediate/aaa">
-              <li className="list-circle   hover:text-customBlue transition-all duration-100 cursor-pointer mt-4">
+              <li className="list-circle   hover:text-customBlue hover:list-disc transition-all duration-100 cursor-pointer mt-4">
                 Complex object
               </li>
             </Link>
             <Link href="intermediate/aaa">
-              <li className="list-circle  hover:text-customBlue transition-all duration-100 cursor-pointer mt-4">
+              <li className="list-circle  hover:text-customBlue hover:list-disc transition-all duration-100 cursor-pointer mt-4">
                 Complex subject
               </li>
             </Link>
             <Link href="intermediate/aaa">
-              <li className="list-circle  hover:text-customBlue transition-all duration-100 cursor-pointer mt-4">
+              <li className="list-circle  hover:text-customBlue hover:list-disc transition-all duration-100 cursor-pointer mt-4">
                 Inversion, ellipsis, substitution
               </li>
             </Link>
