5c73b5c05150181a15345968e512d00cd0e9ff2f
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2022-11-29 15:28:10 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import PageLayout from '../components/layouts/PageLayout';
 export default function Home() {
   return (
     <PageLayout>
-    <div className="flex flex-col justify-start items-center h-full">
+    <div className="flex flex-col justify-start items-center h-full mb-10">
       <div className="h-1/6 pt-5">
           <h1 className="font-segoeBold text-3xl md:text-7xl text-customBlue  text-center pl-10 pr-10 ">
         English grammar
